547453fcf125533a73c2f3f7bd5734cff3af802e
ipunganwar <ipung.anwar1@gmail.com>
2017-11-07 17:46:12 +0700

    penambahan routers/users

--- routers/users.js
+++ routers/users.js
@@ -8,5 +8,37 @@ router.get('/', (req, res) =>{
 	})
 })
 
+router.get('/delete/:id', (req, res)=> {
+	models.User.destroy({
+		where : {id : req.params.id}
+	})
+	.then(() => {
+		res.redirect('/users')
+	})
+})
+
+router.get('/edit/:id', (req, res)=> {
+	models.User.findById(req.params.id).then(user =>{
+		res.render('users/edit', {user, pageTitle:"Edit Users"})
+	})
+
+})
+
+router.post('/edit/:id', (req, res) =>{
+	models.User.update({
+			username : req.body.username,
+			password : req.body.password,
+			first_name : req.body.first_name,
+			last_name : req.body.last_name,
+			email : req.body.email,
+			no_telp : req.body.no_telp
+		},{
+			where: {id : req.params.id}
+		})
+	.then(() => {
+		res.redirect('/users')
+	})
+})
+
 
 module.exports = router
