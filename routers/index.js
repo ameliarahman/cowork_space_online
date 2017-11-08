e2258220c5c9e0b024b8bbef0959fccf4317a1cb
ipunganwar <ipung.anwar1@gmail.com>
2017-11-08 17:05:18 +0700

    login dan session

--- routers/index.js
+++ routers/index.js
@@ -8,6 +8,7 @@ const Sequelize = require('sequelize')
 const op = Sequelize.Op
 
 router.get('/', function (req, res) {
+	console.log(req.session)
     Model.Room.findAll({
         limit: 4
     }).then((dataRooms) => {
@@ -20,13 +21,6 @@ router.get('/', function (req, res) {
     // res.render('index/index', { pageTitle: 'Home' })
 })
 
-router.get('/details/:id', function (req, res) {
-    Model.Room.findById(req.params.id).then((dataRoom) => {
-        res.render('index/details', { pageTitle: "Detail Room", dataRoom: dataRoom })
-    }).catch((reason) => {
-        res.send(reason)
-    })
-})
 
 
 module.exports = router
--- routers/login.js
+++ routers/login.js
@@ -1,19 +1,43 @@
 const express = require('express')
 const router = express.Router()
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
-const myPlaintextPassword = 'ayam';
-
-bcrypt.hash(myPlaintextPassword, saltRounds).then(function(hash) {
-    console.log(hash)
-});
+const models = require('../models')
+const decript = require('../helpers/decript')
 
 router.get('/', (req, res)=> {
+	console.log(req.session)
     res.render('login/login', { pageTitle: 'Login First' })
 })
 
 router.post('/', (req, res) =>{
-	
+
+	models.User.findOne({
+		where: {
+			username : req.body.username
+		}
+	}).then(user =>{
+		
+		if(user){
+			decript(req.body.password, user.password).then(hasil =>{
+				if(hasil){
+					req.session.id = user.id
+					req.session.username = user.username
+					req.session.email = user.email
+					req.session.level = user.level
+					req.session.isLogin = true
+					res.redirect('/')
+				}
+				else{
+					res.render('login/login',{ pageTitle: 'Login First' })
+				}
+			})
+		}
+		else{
+			res.render('login/login',{ pageTitle: 'Login First' })
+		}
+
+	}).catch(error =>{
+		res.send(error)
+	})
 })
 
 
