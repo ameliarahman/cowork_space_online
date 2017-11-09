f661ab37213278f0c66ff8ee250de21eb9e12a94
ameliarahman <amel.rahman5@gmail.com>
2017-11-09 21:06:29 +0700

    Fixing error

--- seeders/20171107101456-dummy-room.js
+++ seeders/20171107101456-dummy-room.js
@@ -17,22 +17,29 @@ module.exports = {
         address : 'Jl.in aja dulu',
         capacity : 20,
         facility : 'spa, pijat++, kolam renang, aqua galon',
-        photo_url : 'https://www.google.co.id/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwj3nNH4nazXAhXFx7wKHe8-DdYQjRwIBw&url=http%3A%2F%2Ftraveler-magazine.blogspot.com%2F2011%2F04%2Fdubai-hotel-7-star-rooms.html&psig=AOvVaw0Kq6HWPK2nnlUQmH6oBacd&ust=1510136215229320',
-        price : '10.000.000'
+        photo_url : 'c9de9d32bb6e5ff2ecb0cfb58e6c25c4',
+        price : '10000'
       },
       {
         address : 'Jl.kemana aja boleh',
         capacity : 25,
         facility : 'pot, asbak, aquarium',
-        photo_url : 'https://www.google.co.id/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwj5oqmtnqzXAhWIgrwKHQ0MDPAQjRwIBw&url=http%3A%2F%2Ftravel.cnn.com%2Fexplorations%2Fescape%2Fworlds-tallest-hotel-reaches-dubai-810539%2F&psig=AOvVaw0Kq6HWPK2nnlUQmH6oBacd&ust=1510136215229320',
-        price : '15.000.000'
+        photo_url : '58bef0974dfecd4bdef01fee99f61d0c',
+        price : '15000'
       },
       {
         address : 'Jl.yang lurus',
         capacity : 10,
         facility : 'pot, asbak, aquarium, banyak lagi yang lainnya',
-        photo_url : 'https://www.google.co.id/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwiv0_PLnqzXAhWEi7wKHdbkAFwQjRwIBw&url=http%3A%2F%2Fdubai.locanto.ae%2FID_575184847%2FBest-and-Cheapest-Deal-of-Labour-camp-in-Al-Quoz.html&psig=AOvVaw0Kq6HWPK2nnlUQmH6oBacd&ust=1510136215229320',
-        price : '35.000.000'
+        photo_url : 'cb38f6a9e0902cc059b530c07efd8cd1',
+        price : '35000'
+      },
+      {
+        address : 'Jl.bareng mantan',
+        capacity : 10,
+        facility : 'pot, asbak, aquarium, banyak lagi yang lainnya',
+        photo_url : 'edf4568fd679558c031ea56b17c1f7bf',
+        price : '15000'
       }
       ], {});
   },
